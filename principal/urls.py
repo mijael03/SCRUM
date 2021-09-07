527e64d93a6f1a0b439a0784384a92cee7374406
Joaquin-1204 <joaquin.salas@tecsup.edu.pe>
2021-09-07 00:24:40 -0500

    correcion

--- principal/urls.py
+++ principal/urls.py
@@ -3,10 +3,10 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.feed, name='feed'),
+    path('', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('register/', views.register, name='register'),
     path('editar/', views.editar, name='editar'),
+    path('feed/', views.feed, name='feed'),
     path('crear/', LoginView.as_view(template_name='principal/crear.html'), name="crear"),
     path('login/', LoginView.as_view(template_name='principal/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='principal/logout.html'), name='logout'),
